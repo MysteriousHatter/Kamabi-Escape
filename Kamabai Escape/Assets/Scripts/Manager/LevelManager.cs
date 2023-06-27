e9d29e479757efbc9ccf0450a12cf51c2d35e260
HerosJourney <Jabari.Alii.Management@gmail.com>
2023-06-27 09:18:10 -0700

    Managers and Menus
    Working on Managers and Menus

---
+++ Kamabai Escape/Assets/Scripts/Manager/LevelManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance { get; private set; }
+
+    public int currentScene;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoToScene(int value)
+    {
+        currentScene = value;
+        SceneManager.LoadScene(value);
+    }
+}
