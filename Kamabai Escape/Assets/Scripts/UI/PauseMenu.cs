bfffb77aba8af5fead3d3466ffe76fcb5b272879
HerosJourney <Jabari.Alii.Management@gmail.com>
2023-07-15 22:39:13 -0700

    Pause Menu
    Pause Menu working with one minor bug, noted in Trello
    
    Basics of score keeping and level saving data has also been put in.

---
+++ Kamabai Escape/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool isGamePaused = false;
+    public GameObject pauseMenuObject;
+    public GameObject[] menuWindows;
+    public int currentLevel;
+    public LevelManager levelManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelManager = GameObject.FindWithTag("LevelManager").GetComponent<LevelManager>();
+        isGamePaused = false;
+        ResumeGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowMenuWindow(int index)
+    {
+        //Hide all menu windows
+        foreach (GameObject obj in menuWindows)
+        {
+            obj.SetActive(false);
+        }
+
+        //Show the appropriate menu window
+        menuWindows[index].SetActive(true);
+    }
+
+    public void TogglePauseGame()
+    {
+        if (isGamePaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        isGamePaused = true;
+        pauseMenuObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        isGamePaused = false;
+        pauseMenuObject.SetActive(false);
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1;
+        isGamePaused = false;
+        levelManager.currentLevel = currentLevel;
+        levelManager.SaveData();
+        SceneManager.LoadScene(0);
+    }
+
+}
